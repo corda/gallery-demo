f478ab95028a0700eda48ff89b9e2c77bc208eba
Anthony Nixon <anixon604@gmail.com>
2021-08-06 19:26:06 +0100

    Workflows module set

---
+++ gallery-workflows/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("net.corda.plugins.cordapp")
+    id("net.corda.plugins.quasar-utils")
+}
+
+val cordaPlatformVersion : String by project
+val cordaCoreReleaseGroup : String by project
+val cordaCoreVersion : String by project
+val gradlePluginsVersion : String by project
+
+cordapp {
+    val cordaPlatformVersion : String by rootProject
+    val cordaLedgerReleaseVersion : String by rootProject
+
+    targetPlatformVersion(cordaPlatformVersion.toInt())
+    minimumPlatformVersion(cordaPlatformVersion.toInt())
+    contract {
+        name("Gallery Workflows")
+        vendor("Corda Open Source")
+        licence("Apache License, Version 2.0")
+        versionId(cordaLedgerReleaseVersion.toInt())
+    }
+}
+
+dependencies {
+    cordaCompile(cordaCoreReleaseGroup,"corda-core", cordaCoreVersion)
+    cordaRuntime(cordaCoreReleaseGroup, "corda", cordaCoreVersion)
+    testImplementation(cordaCoreReleaseGroup, "corda-node-driver", cordaCoreVersion)
+
+    cordapp(project(":gallery-contracts"))
+}
