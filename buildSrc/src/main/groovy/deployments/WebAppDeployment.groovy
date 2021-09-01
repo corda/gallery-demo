2ed9331320314508292807423c71012117b079ae
Anthony Nixon <anixon604@gmail.com>
2021-09-01 19:18:08 +0100

    WebAppDeployment cleaned.

--- buildSrc/src/main/groovy/deployments/WebAppDeployment.groovy
+++ buildSrc/src/main/groovy/deployments/WebAppDeployment.groovy
@@ -60,94 +60,6 @@ class WebAppDeployment implements Iterable<Object> {
                                                   Integer webAppPort = 8080 // server port for spring config and service
     ) {
 
-        V1PersistentVolumeClaim postgresDataPVC = new V1PersistentVolumeClaimBuilder()
-                .withKind("PersistentVolumeClaim")
-                .withApiVersion("v1")
-                .withNewMetadata()
-                .withNamespace(devNamespace)
-                .withName("${apiId}-postgres-data-pvc")
-                .endMetadata()
-                .withNewSpec()
-                .withAccessModes("ReadWriteOnce")
-                .withNewResources()
-                .withRequests([storage: new Quantity("1Gi")])
-                .endResources()
-                .endSpec()
-                .build()
-
-        def postgresDeployment = new V1DeploymentBuilder()
-                .withKind("Deployment")
-                .withApiVersion("apps/v1")
-                .withNewMetadata()
-                .withName("${apiId}-webappdb")
-                .withNamespace(devNamespace)
-                .endMetadata()
-                .withNewSpec()
-                .withNewSelector()
-                .withMatchLabels([run: "${apiId}-webappdb".toString()])
-                .endSelector()
-                .withReplicas(1)
-                .withNewTemplate()
-                .withNewMetadata()
-                .withLabels([run: "${apiId}-webappdb".toString()])
-                .endMetadata()
-                .withNewSpec()
-                .withVolumes(
-                        new V1VolumeBuilder().withName("${apiId}-postgres-data-storage")
-                                .withPersistentVolumeClaim(
-                                        new V1PersistentVolumeClaimVolumeSource().claimName(postgresDataPVC.metadata.name)
-                                ).build()
-                )
-                .addNewInitContainer()
-                .withName("${apiId}-postgres-chown-data")
-                .withImage("busybox:latest")
-                .withImagePullPolicy("IfNotPresent")
-                .withCommand("chown")
-                .withArgs("-Rv", "999:999", "/var/lib/postgresql/data")
-                .withVolumeMounts(
-                        new V1VolumeMountBuilder().withName("${apiId}-postgres-data-storage").withMountPath("/var/lib/postgresql/data").build()
-                )
-                .endInitContainer()
-                .addNewContainer()
-                .withName("${apiId}-webappdb")
-                .withImage("postgres:9.6")
-                .withImagePullPolicy("IfNotPresent")
-                .withEnv(new V1EnvVarBuilder().withName("POSTGRES_PASSWORD").withValue("postgres").build())
-                .withPorts(
-                        new V1ContainerPortBuilder().withName("postgresport").withContainerPort(5432).build()
-                )
-                .withVolumeMounts(
-                        new V1VolumeMountBuilder()
-                                .withName("${apiId}-postgres-data-storage")
-                                .withSubPath("postgres")
-                                .withMountPath("/var/lib/postgresql/data").build()
-                )
-                .withNewResources()
-                .withRequests("memory": new Quantity("512Mi"), "cpu": new Quantity("0.5"))
-                .endResources()
-                .endContainer()
-                .endSpec()
-                .endTemplate()
-                .endSpec()
-                .build()
-
-        def postgresService = new V1ServiceBuilder()
-                .withKind("Service")
-                .withApiVersion("v1")
-                .withNewMetadata()
-                .withNamespace(devNamespace)
-                .withName("${apiId}-webappdb")
-                .withLabels([run: "${apiId}-webappdb".toString()])
-                .endMetadata()
-                .withNewSpec()
-                .withPorts(
-                        new V1ServicePortBuilder()
-                                .withPort(5432).withProtocol("TCP").withName("postgresport").build()
-                ).withSelector([run: "${apiId}-webappdb".toString()])
-                .endSpec()
-                .build()
-
-
         def webappDeployment = new V1DeploymentBuilder()
                 .withKind("Deployment")
                 .withApiVersion("apps/v1")
@@ -237,6 +149,4 @@ class WebAppDeployment implements Iterable<Object> {
         //we do not need webapp DB just yet.
         return new WebAppDeployment(webappDeployment, webappHttpService, webappDebugService)
     }
-
-
 }
