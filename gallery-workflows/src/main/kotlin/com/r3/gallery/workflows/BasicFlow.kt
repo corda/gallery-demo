64f7e8da5b4896dcf9b0139e8144c3a0bb25743d
Anthony Nixon <anixon604@gmail.com>
2021-09-08 22:17:53 +0100

    Progress tracking added to basic test flow.

--- gallery-workflows/src/main/kotlin/com/r3/gallery/workflows/BasicFlow.kt
+++ gallery-workflows/src/main/kotlin/com/r3/gallery/workflows/BasicFlow.kt
@@ -7,26 +7,58 @@ import net.corda.core.flows.*
 import net.corda.core.identity.Party
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
+import net.corda.core.utilities.ProgressTracker
 
 @StartableByRPC
 @InitiatingFlow
 class BasicFlowInitiator(private val participant: Party) : FlowLogic<SignedTransaction>() {
+
+    companion object {
+        object GENERATING_TRANSACTION : ProgressTracker.Step("Generating transaction based on new IOU.")
+        object VERIFYING_TRANSACTION : ProgressTracker.Step("Verifying contract constraints.")
+        object SIGNING_TRANSACTION : ProgressTracker.Step("Signing transaction with our private key.")
+        object GATHERING_SIGS : ProgressTracker.Step("Gathering the counterparty's signature.") {
+            override fun childProgressTracker() = CollectSignaturesFlow.tracker()
+        }
+
+        object FINALISING_TRANSACTION : ProgressTracker.Step("Obtaining notary signature and recording transaction.") {
+            override fun childProgressTracker() = FinalityFlow.tracker()
+        }
+
+        fun tracker() = ProgressTracker(
+            GENERATING_TRANSACTION,
+            VERIFYING_TRANSACTION,
+            SIGNING_TRANSACTION,
+            GATHERING_SIGS,
+            FINALISING_TRANSACTION
+        )
+    }
+
+    override val progressTracker = tracker()
 
     @Suspendable
     override fun call(): SignedTransaction {
         val notary = serviceHub.networkMapCache.notaryIdentities[0]
         val output = EmptyState(listOf(participant, ourIdentity))
 
+        progressTracker.currentStep = GENERATING_TRANSACTION
+
         val transactionBuilder = TransactionBuilder(notary)
             .addCommand(EmptyContract.Commands.Create(), participant.owningKey, ourIdentity.owningKey)
             .addOutputState(output)
 
+        progressTracker.currentStep = VERIFYING_TRANSACTION
+
         transactionBuilder.verify(serviceHub)
+
+        progressTracker.currentStep = SIGNING_TRANSACTION
         val ptx = serviceHub.signInitialTransaction(transactionBuilder)
 
+        progressTracker.currentStep = GATHERING_SIGS
         val counterPartySession = listOf(initiateFlow(participant))
         val stx = subFlow(CollectSignaturesFlow(ptx, counterPartySession))
 
+        progressTracker.currentStep = FINALISING_TRANSACTION
         return subFlow(FinalityFlow(stx, counterPartySession))
     }
 
