023f38f90b446b357a1db7e6f696a9cfd497a820
Peter Bienek <peter.bienek@r3.com>
2021-09-29 12:28:00 +0100

    Adds inexplicably missing Backdrop code.

---
+++ frontend/galleryapp/r3-tooling-design-system/lib/components/Atoms/Backdrop/Backdrop.js
@@ -0,0 +1,29 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+import { jsx as _jsx } from "react/jsx-runtime";
+var Backdrop = function (_a) {
+    var show = _a.show, onClick = _a.onClick, otherProps = __rest(_a, ["show", "onClick"]);
+    return show ? (_jsx("div", __assign({ className: "w-full h-full fixed left-0 top-0 bg-dark-gray-400 opacity-50 z-40", onClick: onClick }, otherProps), void 0)) : null;
+};
+export default Backdrop;
+//# sourceMappingURL=Backdrop.js.map
